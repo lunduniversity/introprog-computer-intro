d6d6438d33aca41558754ac9411ffcae74d32112
Mattias Nordahl <mattias.nordahl@cs.lth.se>
2024-09-15 20:44:06 +0200

    Big update on LaTeX slides, still WIP

---
+++ lab-material/latex_new/sort.scala
@@ -0,0 +1,33 @@
+object SortComparison {
+  def quickSort(arr: Array[Int]): Array[Int] = {
+    if (arr.length <= 1) arr
+    else {
+      val pivot = arr(arr.length / 2)
+      Array.concat(
+        quickSort(arr.filter(_ < pivot)),
+        arr.filter(_ == pivot),
+        quickSort(arr.filter(_ > pivot))
+      )
+    }
+  }
+
+  def mergeSort(arr: Array[Int]): Array[Int] = {
+    if (arr.length <= 1) arr
+    else {
+      val (left, right) = arr.splitAt(arr.length / 2)
+      val sortedLeft = mergeSort(left)
+      val sortedRight = mergeSort(right)
+      merge(sortedLeft, sortedRight)
+    }
+  }
+
+  private def merge(left: Array[Int], right: Array[Int]): Array[Int] = {
+    (left, right) match {
+      case (Array(), _) => right
+      case (_, Array()) => left
+      case (l, r) =>
+        if (l.head < r.head) l.head +: merge(l.tail, r)
+        else r.head +: merge(l, r.tail)
+    }
+  }
+}
